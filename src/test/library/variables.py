a539e5018d853c83423ae1dc97e58ae8eab0451d
lindalopezc <lindalopezramos61@gmail.com>
2024-06-24 16:25:58 -0500

    chore: modify variables.py

--- src/test/library/variables.py
+++ src/test/library/variables.py
@@ -5,7 +5,3 @@ html_for_each_feature_template=os.getcwd()+"\\src\\test\\resources\\template\\re
 report_folder_path = os.getcwd()+"\\src\\test\\reports"
 template_folder_path = os.getcwd()+"\\src\\test\\resources\\template"
 source_files = ["logo_mibanco.png", "main.js", "style.css", "icon.ico"]
-
-excel_path = os.getcwd()+"\\src\\test\\resources\\data\\excel\\test_data.xlsx" #TO DO
-expected_columns = ["Expected Status Code",	"Expected Content_1", "Expected Content_2",
-                        "Expected Content_3", "Expected Content_4",	"Expected Content_5", "Expected Content_6"]
